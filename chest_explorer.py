cb9bfb9d3bfee38a8a789dcd1cbaad94f40ee2a7
colonbrac3t <ludoattuoni@gmail.com>
2022-11-03 10:32:50 +0000

    fixed dubloonage

--- chest_explorer.py
+++ chest_explorer.py
@@ -23,6 +23,7 @@ regex_spider_string =r'"spider": {[\\\\r\\\\n "a-z:,]+}'
 regex_boots_string = r'"boots": {[\\\\r\\\\n "a-z:A-Z.,0-9]+}'
 regex_alive_string = r'"alive":[ ]+(\w+)'
 regex_size_string = r'"size":[ ]+(\w+)'
+regex_value_string = r'"value": ([0-9]+)'
 def look_through_chests(url):
     #print('Begin searching in',url)
     string_chests = str(get_string_chest(url))
@@ -38,13 +39,6 @@ def look_through_chests(url):
         for match in matches:
             num_mineral = int(re.findall('[0-9]+',match)[0])
             dubloons += num_mineral * value
-        '''
-        def finder_function(match_json):
-            global dubloons
-            mineral = match_json[mineral_string]['count']
-            dubloons += mineral * value
-        regex_finder(mineral_regex, finder_function)
-        '''  
     def look_for_dead_spiders():
         global dead_spiders
         matches = re.findall(regex_spider_string,string_chests)
@@ -58,9 +52,15 @@ def look_through_chests(url):
         for match in matches:
             size = int(re.findall(regex_size_string,match,re.M)[0])
             boot_sizes[size]+=1
+    def collect_all_the_monies():
+        global dubloons
+        matches = re.findall(regex_value_string, string_chests)
+        for match in matches:
+            dubloons += int(match)
     rare_mineral_finder('diamond',regex_diamond_string,400)
     rare_mineral_finder('ruby', regex_ruby_string,250)
     rare_mineral_finder('sapphire', regex_sapphire_string,200)
+    collect_all_the_monies()
     look_for_dead_spiders()
     look_for_boots()
     if 'holy-grail' in string_chests:
